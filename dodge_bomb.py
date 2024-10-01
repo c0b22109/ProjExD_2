0e9f549d742e2d5ee97b88bace63c2f63805520d
C0B22109 <c0b2210962@edu.teu.ac.jp>
2024-10-01 15:16:08 +0900

    練習4

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -50,7 +50,10 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-        screen.blit(bg_img, [0, 0]) 
+        screen.blit(bg_img, [0, 0])
+
+        if kk_rct.colliderect(bb_rct):
+            return
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
